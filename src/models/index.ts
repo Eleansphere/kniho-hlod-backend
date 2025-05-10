5e0248ec84c2889e5d019d0b2950e8fb0188e1ef
Vojta Franek <vojtech.franek@livesystems.cz>
2025-05-10 15:10:48 +0200

    disable associations

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,8 +2,6 @@ import sequelize from './sequelize';
 import Book from './Book';
 import User from './User';
 import Loans from './Loan';
-import { defineAssociations } from './associations';
-defineAssociations();
 
 
 export { sequelize, Book, User, Loans };
