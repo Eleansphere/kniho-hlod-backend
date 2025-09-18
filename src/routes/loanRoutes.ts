ad3268b1b860a6a08b21f9f458e8afb0c7783d49
Vojta Franek <franek.vojtech01@gmail.com>
2025-09-18 22:13:22 +0200

    add PUT method to Loan

--- src/routes/loanRoutes.ts
+++ src/routes/loanRoutes.ts
@@ -26,6 +26,29 @@ router.post('/', async (req: Request, res: Response) => {
     }
 });
 
+// PUT request
+router.put('/:id', async (req: Request, res: Response) => {
+    try {
+        const loan = await Loan.findByPk(req.params.id);
+        if (!loan) {
+            return res.status(404).json({ message: 'Půjčka nebyla nalezena' });
+        }
+
+        await loan.update({
+            borrower: req.body.borrower,
+            loanDate: req.body.loanDate,
+            returnDate: req.body.returnDate,
+            bookId: req.body.bookId,
+            ownerId: req.body.ownerId,
+            isReturned: req.body.isReturned
+        });
+
+        res.status(200).json(loan);
+    } catch (err) {
+        res.status(500).json({ error: (err as Error).message });
+    }
+});
+
 // GET request all loans
 router.get('/', async (req: Request, res: Response) => {
     try {
