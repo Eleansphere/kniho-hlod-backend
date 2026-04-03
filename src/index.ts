e7d91ea4fdc588eb79d62a53557a697b0c8d5a65
Vojta Franek <vojtech.franek@livesystems.cz>
2026-04-03 17:03:13 +0200

    fix: build error

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { createApp } from '@eleansphere/be-core';
-import { bookConfig, loanConfig, profileImageConfig } from '@eleansphere/kniho-hlod-service';
+import { bookEntity, loanEntity, profileImageEntity } from '@kniho-hlod/kniho-hlod-service';
 import { plugin } from './plugin';
 
 createApp({
@@ -10,9 +10,9 @@ createApp({
   jwtSecret: process.env.JWT_SECRET!,
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   modelConfigs: [
-    { ...bookConfig, log: true },
-    { ...loanConfig, log: true },
-    { ...profileImageConfig, log: true },
+    {...bookEntity.config, log: true },
+    { ...loanEntity.config, log: true },
+    { ...profileImageEntity.config, log: true },
   ],
   plugins: [plugin],
   auth: { modelName: 'user' },
